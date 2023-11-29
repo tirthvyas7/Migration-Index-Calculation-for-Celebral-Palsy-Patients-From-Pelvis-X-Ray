6c364adaf721c0ad9150833b226c06e87cc12e6b
Tirth Vyas <132454970+tirthvyas7@users.noreply.github.com>
2023-11-30 00:28:58 +0530

    Update code.py

--- code.py
+++ code.py
@@ -22,52 +22,52 @@ cv2.waitKey(0)
 
 
 
-
+#Initial Thresholding to segment out the bones
 gray=cv2.cvtColor(imS,cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (9, 9), 2)
-lower_th = 100
-upper_th = 150
+lower_th = 100 #The values are to be tuned for different images
+upper_th = 150 #The values are to be tuned for different images
 th_mask = cv2.inRange(blurred, lower_th, upper_th)
 thresh = cv2.bitwise_and(imS, imS, mask=th_mask)
 
+#Cropping for Calculation of A value
 r1= cv2.selectROI("Crop Femoral Head and Acetabelum for 'A' Measurement", thresh)
 cropA=thresh[int(r1[1]):int(r1[1]+r1[3]),int(r1[0]):int(r1[0]+r1[2])] 
 # cv2.imshow("Cropped",cropA) 
 # cv2.waitKey(0)
-
-
-
 
 corner=cropA.copy()
 img=cv2.cvtColor(imS,cv2.COLOR_BGR2GRAY)
 crop=cv2.cvtColor(cropA,cv2.COLOR_BGR2GRAY)
 
 #Harris Croner Detection
-
 gray = np.float32(crop.copy())
 dst = cv2.cornerHarris(gray,3,3,0.04)
 #result is dilated for marking the corners, not important
 dst = cv2.dilate(dst,None)
 # Threshold for an optimal value, it may vary depending on the image.
 corner[dst>0.01*dst.max()]=[0,0,255]
+#Giving Option for Doctor to select the most preferable Corner from multiple detected corners
 cv2.imshow('Click on the detected corner of acetabelum and left most corner of femoral head',corner)
 cv2.setMouseCallback('Click on the detected corner of acetabelum and left most corner of femoral head', click_event)
-
 if cv2.waitKey(0) & 0xff == 27:
     cv2.destroyAllWindows()
 A=abs(xcord[0]-xcord[1])
 print("A=",A)
 
+#Cropping for Calculation of B value
 r= cv2.selectROI('Crop Femoral Head for "B"', thresh)
 cropB=thresh[int(r[1]):int(r[1]+r[3]),int(r[0]):int(r[0]+r[2])] 
 # cv2.imshow("Femoral Head for 'B'",cropB) 
 # cv2.waitKey(0)
 
+#Finding Contours in the Cropped Image
 cropB=cv2.cvtColor(cropB,cv2.COLOR_BGR2GRAY)
 contours, _ = cv2.findContours(cropB.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cont=cv2.cvtColor(cropB,cv2.COLOR_GRAY2RGB).copy()
-
 cont=cv2.drawContours(cont, contours,-1,color=(255, 0, 0),thickness= 2)
+
+#Finding Maximum Area Bounding Box and Plotting on Image
 c=max(contours,key=cv2.contourArea)
 x,y,w,h=cv2.boundingRect(c)
 B=w
